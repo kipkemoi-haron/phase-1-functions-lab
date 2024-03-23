d2df54874b39246b54730965c91ee9d84263b97a
kipkemoi Haron <kipkemoiharon4@gmail.com>
2024-03-23 15:06:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function calculatesFarePrice(start, destination)
+
+
+// const { describe, it } = require('mocha'); 
+// const { expect } = require('chai');
+// const distanceFromHqInBlocks = require('./index');
+
+//the file being tested
+describe('index.js', function() {
+  
+  //the function being tested
+  describe('distanceFromHqInBlocks()', function() {
+    
+    //what the function does
+    it('returns a distance in blocks', function() {
+      
+      // Test case:
+      expect(distanceFromHqInBlocks(43)).to.equal(1);
+    });
+    
+  });
+
